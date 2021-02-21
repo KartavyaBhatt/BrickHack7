9c0da0902a3bbfd677d7400107630198efcf0f46
Kartavya Bhatt <kartavya075@gmail.com>
2021-02-21 03:35:24 -0500

    mongodb + wegmans api

---
+++ KB/dbConn.py
@@ -0,0 +1,17 @@
+import pymongo
+from pymongo import MongoClient
+from interface import API
+
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+
+# Debugging
+db = client['productdb']
+col = db['product']
+col.delete_many({})
+
+class MongoDB:
+    def insertItem(self, barcode):
+        db = client['productdb']
+        col = db['product']
+        doc = col.insert_one(API().addItem(barcode))
---
+++ KB/interface.py
@@ -0,0 +1,41 @@
+import requests
+
+class API:
+    def addItem(self, barcode):
+        payload={}
+        headers = {}
+
+        # barcode = '077890443835'
+        url = "https://api.wegmans.io/products/barcodes/"+barcode+"?api-version=2018-10-18&subscription-key=d39826cb6b7449279dda03f8b4c70843"
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = response.json()
+        sku = data['sku']
+        name = data['name']
+
+        url = "https://api.wegmans.io/products/" + str(sku) +"?api-version=2018-10-18&subscription-key=d39826cb6b7449279dda03f8b4c70843"
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = response.json()
+        for item in data["nutrients"]:
+            if item["type"] == "Calories":
+                calories = item['quantity']
+                break
+
+        for item in data["tradeIdentifiers"]:
+            if item["images"] is not None:
+                img = item['images'][0]
+                break
+
+        url = "https://api.wegmans.io/products/"+ str(sku) +"/prices/25?api-version=2018-10-18&subscription-key=d39826cb6b7449279dda03f8b4c70843"
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = response.json()
+        price = data["price"]
+        quantity = 1
+
+        data = {'_id': sku,
+        'pName': name,
+        'pCal': calories,
+        'pPrice': price,
+        'pQuantity': quantity,
+        'pImage': img}
+        print(barcode, sku, name)
+        return data
