7943942c1f348c572aebf5fadaeca5625a0f6d3f
ManjulSinghSachan <sachanmanjul@gmail.com>
2021-02-21 04:25:00 -0500

    Pulling data from MongoDB through flask and using in UI

--- flask_init.py
+++ flask_init.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/productdb"
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = mongo.db.product.find({})
+    print ("Items: ", items)
+    return render_template("index.html", items=items)
