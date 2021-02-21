1f06b7010b64efb48f777aa4ccb6e75e28beb065
ManjulSinghSachan <sachanmanjul@gmail.com>
2021-02-21 07:06:47 -0500

    Changes to recently added iem

--- flask_init.py
+++ flask_init.py
@@ -1,5 +1,11 @@
+# from sshtunnel import SSHTunnelForwarder
+
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+
+# server = SSHTunnelForwarder("192.168.0.21", "productdb", remote_bind_address=('127.0.0.1', 5000))
+
+# server.start()
 
 app = Flask(__name__)
 
@@ -9,5 +15,7 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     items = mongo.db.product.find({})
-    print ("Items: ", items)
-    return render_template("index.html", items=items)
+    first_item = mongo.db.product.find().sort("_id", -1).limit(1).__getitem__(0)
+    return render_template("index.html", items=items, first_item=first_item)
+
+# server.stop();
